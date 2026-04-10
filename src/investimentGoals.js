f41cbb2cbe0aa278dd291a312cc65bd634c66762
Ronei José Roteski <pokemonronei@yahoo.com.br>
2026-04-10 02:16:32 -0300

    Formulario de investimento e arquivo de cálculo de projeções criado

---
+++ src/investimentGoals.js
@@ -0,0 +1,61 @@
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFreme = "monthly",
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      "Investimento inicial e prazo devem ser preenchidos com valores positivos.",
+    );
+  }
+  const finalReturnRate =
+    returnTimeFreme === "monthly"
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmonunt: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+
+    const interestReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+
+    const totalInterestReturns = totalAmount - investedAmount;
+
+    returnsArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+
+  return returnsArray;
+}
